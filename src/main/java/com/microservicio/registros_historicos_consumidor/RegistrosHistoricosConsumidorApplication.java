7868a869182b4400e84a0411fb666b45877ae3b5
Manuel Valdés <manuelvaldesg97@gmail.com>
2025-02-22 18:24:42 -0300

    Creación microservicio java spring boot con integración a rabbitmq, consumidor.

---
+++ src/main/java/com/microservicio/registros_historicos_consumidor/RegistrosHistoricosConsumidorApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.registros_historicos_consumidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrosHistoricosConsumidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrosHistoricosConsumidorApplication.class, args);
+	}
+
+}
